e77c3a1f94b6f8878f125c916a4918f84fa2a4d7
Hanest <53016199+Hanestx@users.noreply.github.com>
2026-01-22 02:35:12 +0400

    feat: add movement component

---
+++ Assets/_Project/Components/MovementComponent.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovementComponent : MonoBehaviour
+{
+    public float MoveSpeed => _moveSpeed;
+    public Rigidbody Rigidbody => _rigidbody;
+
+    
+    [SerializeField] private float _moveSpeed = 6f;
+
+    
+    private Rigidbody _rigidbody;
+
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _rigidbody.useGravity = false;
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+}
