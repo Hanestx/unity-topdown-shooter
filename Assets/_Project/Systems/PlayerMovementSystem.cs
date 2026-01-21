3039060213b3c137ee2854d7c7d2bbe31c94f290
Hanest <53016199+Hanestx@users.noreply.github.com>
2026-01-22 02:35:34 +0400

    feat: implement player movement system

---
+++ Assets/_Project/Systems/PlayerMovementSystem.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovementSystem : MonoBehaviour
+{
+    [SerializeField] private MovementComponent _movement;
+    [SerializeField] private Transform _playerTransform;
+
+    
+    private void FixedUpdate()
+    {
+        if (_movement == null)
+            return;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new(horizontal, 0f, vertical);
+
+        Move(direction);
+        Rotate(direction);
+    }
+
+    
+    private void Move(Vector3 direction)
+    {
+        Vector3 velocity = direction * _movement.MoveSpeed;
+        _movement.Rigidbody.linearVelocity =
+            new Vector3(velocity.x, _movement.Rigidbody.linearVelocity.y, velocity.z);
+    }
+
+    private void Rotate(Vector3 direction)
+    {
+        if (direction.sqrMagnitude < 0.001f)
+            return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        _playerTransform.rotation = Quaternion.Slerp(
+            _playerTransform.rotation,
+            targetRotation,
+            Time.fixedDeltaTime * 15f
+        );
+    }
+}
